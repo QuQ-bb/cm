f336cfb4ad17bd6bd1bb8dba5a204c58b0403a33
ARmeria0801 <dkfldkfl0801@gmail.com>
2021-11-22 07:29:34 +0900

    qnaWrite

---
+++ src/main/java/com/myshop/cm/controller/StoreController.java
@@ -0,0 +1,30 @@
+package com.myshop.cm.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.myshop.cm.model.GoodsVO;
+import com.myshop.cm.service.StoreService;
+
+@Controller
+@RequestMapping("/store/**")
+public class StoreController {
+
+	@Inject
+	private StoreService storeService;
+	
+	//상품 목록
+	@GetMapping("storeList")
+	public void storeList(GoodsVO goodsVO, Model model) throws Exception {
+		
+//		List<GoodsVO> list = storeService.
+		
+		
+	}
+}
