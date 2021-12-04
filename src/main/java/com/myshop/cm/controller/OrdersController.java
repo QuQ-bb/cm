c119c94feaaa36dfecfb7361e590a1fbfd481c55
ARmeria0801 <dkfldkfl0801@gmail.com>
2021-12-04 19:26:26 +0900

    order

--- src/main/java/com/myshop/cm/controller/OrdersController.java
+++ src/main/java/com/myshop/cm/controller/OrdersController.java
@@ -1,6 +1,7 @@
 package com.myshop.cm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/orders/**")
 public class OrdersController {
 
+	//주문 목록
 	@GetMapping("ordersList")
-	public void ordersList() throws Exception {
+	public void ordersList(Model model) throws Exception {
 		System.out.println("OrdersList 들어왔냐");
+		
+		
 	}
 }
